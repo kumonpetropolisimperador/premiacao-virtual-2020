bc243275eeefba6e346cb16c1c42f32a41ea129c
Eltonleao <3lton.leao@gmail.com>
2020-12-25 21:53:10 -0300

    area de comentarios

--- js/main.js
+++ js/main.js
@@ -1,41 +1,105 @@
+var comentarios = db.collection('premiacao-2020-comentarios');
 $(document).ready(function () {
+
     console.log('running...');
 
-    $("#comentarios-form").submit(function (e) {
-        e.preventDefault();
-        Swal.showLoading();
-        $.ajax({
-            method: "POST",
-            url: "https://cors-anywhere.herokuapp.com/http://api-elton.herokuapp.com/kumonPremiacao/sendComentario",
-            data: $(this).serialize(),
-            success: function (data) {
-                console.log(data);
-                Swal.fire("Sucesso!","Seu comentário foi enviado com sucesso! Em breve ele aparecerá na listagem!", "success");
-                Swal.hideLoading();
-            }
-        });
-    });
+    // $("#comentarios-form").submit(function (e) {
+    //     e.preventDefault();
+    //     Swal.showLoading();
+
+    //     var formData = {};
+    //     $.each($(this).serializeArray(), function (_, kv) {
+    //         formData[kv.name] = kv.value;
+    //     });
+
+    //     comentarios.add({
+    //         nome: formData.nome,
+    //         email: formData.email,
+    //         texto: formData.texto,
+    //         data_criacao: new Date(),
+    //         verificado: 0,
+    //         deletado: 0
+    //     }).then(function () {
+    //         Swal.hideLoading();
+    //         Swal.fire("Sucesso!", "Seu comentário foi enviado com sucesso! Em breve ele aparecerá na listagem!", "success");
+    //     });
+
+    // });
 
 
-    function getComentarios(){
-        $.ajax({
-            method: "GET",
-            url: "http://api-elton.herokuapp.com/kumonPremiacao/getComentarios",
-            success: function (data) {
-                console.log(data);
-                var html = "";
-                html += '<div class="card">';
-                html +='<div class="card-body">';
-                html += '<h5 class="card-title">Card title</h5>';
-                html += '<h6 class="card-subtitle mb-2 text-muted">24/12/2020</h6>';
-                html += '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>';
-                html += '</div>'
-                html += '</div>'
+    function getComentarios() {
 
-                // $("#lista-comentarios").html(html);
-            }
-        });
+        comentarios.where('deletado', '==', 0).where('verificado', '==', 1).get().then(function (e) {
+            console.log(e.docs);
+            var html = "";
+            e.docs.forEach(item => {
+                var item = item.data();
+                var data_criacao = item.data_criacao;
+                // console.log(new Date(data_criacao.seconds * 1000));
+                data_criacao = new Date(data_criacao.seconds * 1000);
+                html += '<div class="card mb-15">';
+                html += '<div class="card-body">';
+                html += '<h5 class="card-title">' + item.nome + '</h5>';
+                html += '<h6 class="card-subtitle mb-2 text-muted">' + data_criacao.getDate() + '/' + data_criacao.getMonth() + '/' + data_criacao.getFullYear() + '</h6>';
+                html += '<p class="card-text">' + item.texto + '</p>';
+                html += '</div>';
+                html += '</div>';
+            });
+            $("#lista-comentarios").html(html);
+        })
     }
-
     getComentarios();
+
 });
+
+function fazerComentario() {
+    Swal.mixin({
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Próximo',
+        showCancelButton: true,
+        progressSteps: ['1', '2', '3']
+    }).queue([
+        {
+            input: 'text',
+            title: 'Seu nome',
+            required: "required",
+            validationMessage: "Oops, algo errado com seu nome. Está correto mesmo?"
+        },
+        {
+            input: 'email',
+            title: 'Seu email',
+            attributes: {
+                required: 'required'
+            },
+            validationMessage: "Oops, algo errado com seu email. Está correto mesmo?",
+        },
+        {
+            input: 'text',
+            attributes: {
+                required: 'required'
+            },
+            validationMessage: "Oops, algo errado com seu texto. Está correto mesmo?",
+            title: 'Seu comentário!'
+
+        }
+    ]).then((result) => {
+        if (result.value) {
+            answers = result.value;
+            Swal.showLoading();
+
+            comentarios.add({
+                nome: answers[0],
+                email: answers[1],
+                texto: answers[2],
+                data_criacao: new Date(),
+                verificado: 0,
+                deletado: 0
+            }).then(function () {
+                Swal.hideLoading();
+                Swal.fire("Sucesso!", "Seu comentário foi enviado com sucesso! Em breve ele aparecerá na página!", "success");
+            });
+        }
+        $('.swal2-input').attr('required', 'required');
+    });
+    $('.swal2-input').attr('required', 'required');
+}
