52d9fd11b1c416bb32a4400a50b78c61d225b341
Eltonleao <3lton.leao@gmail.com>
2020-12-30 11:46:28 -0300

    timer

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         toast: true,
         position: 'top-end',
         showConfirmButton: false,
-        timer: 2000,
+        timer: 4000,
         didOpen: (toast) => {
             toast.addEventListener('mouseenter', Swal.stopTimer)
             toast.addEventListener('mouseleave', Swal.resumeTimer)
